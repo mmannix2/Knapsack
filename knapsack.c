084bbb46cddfcb933f576211b4fb66f5f84cb96a
Matthew Mannix <m.mannix3@gmail.com>
2016-09-16 16:25:16 -0400

    Write calcweight().

--- knapsack.c
+++ knapsack.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <limits.h>
 
 #define DEBUG
 
-#define MAX_NUM_ITEMS 1000
+#define MAX_NUM_ITEMS 32 //Max of 32 Items because 2^32 = UINT_MAX
 
 struct Item {
     int value;
@@ -13,7 +14,33 @@ struct Item {
 
 int weight_limit = -1;
 struct Item items[MAX_NUM_ITEMS]; 
-int num_items = 0;
+int num_items = -1;  //Number of Items
+unsigned int num_combos = 0; //2^(num_items)
+
+/* Calculates the value of one set of items
+ * Returns the value of the items if the weight does not exceed weight_limit.
+ * Returns -1 if the given combination is not valid for the Item set.
+ *
+ * NOTE: weight_limit must be given a value before this is called!
+ */
+int calcvalue(unsigned int combination, int* value) {
+    if(combination >= num_combos) {
+        return -1;
+    }
+    else {
+        *value = 0;
+        for(int i = 0; i < num_items; i++) {
+            //printf("\tItem #%d, value: %d\n", i, items[i].value);
+            //printf("\tCombo: %u Bit: %d\n", combination, combination % 2);
+            if(combination % 2 != 0) {
+                *value += items[i].value;
+            }
+            combination >>= 1;
+        }
+        //printf("Items done. value: %d\n", *value);
+        return 0;
+    }
+}
 
 /* Calculates the solution to the knapsack problem using a brute force approach
  * distributed over a given number of threads.
@@ -49,15 +76,20 @@ int main(int argc, char** argv) {
             fscanf(file,"%d %d", &items[i].value, &items[i].weight);
         }
         num_items = i-1;
+        num_combos = 0b1;
+        for(int i = 0; i < num_items; i++) {
+            num_combos *= 2; 
+        }
     }
-   
-    #ifdef DEBUG
-    printf("Weight limit: %d\n", weight_limit);
-    printf("Items:\n");
-    for(int i=0; i<num_items; i++) {
-        printf("\tItem #%d, v=%d w=%d\n", i, items[i].value, items[i].weight);
+    
+    printf("Number of items: %d\n", num_items);
+    printf("Number of combinations: %u\n", num_combos);
+    
+    int v = 0;
+    for(unsigned int i = 0; i < num_combos; i++) {
+        calcvalue(i, &v);
+        printf("Combo: %u Value: %d\n", i, v);
     }
-    #endif
 
     return 0;
 }
